8327d86a8456d420442f5d9c87388c3958a07a5e
wellasomething <swella000@gmail.com>
2022-12-31 03:05:31 +0100

    co

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Union from './Union.png';
+import pablo from './pablo.svg';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="logo-maker">
+    <img src={Union} alt=""  className="logo" />
+    <h3 className="logo">lendersqr</h3>
+</div>
+
+<div className="login">
+    <div className="Two">
+      
+      <div>
+        <img src={pablo} alt="" />
+      </div>
     </div>
+
+    <div className="Two">
+        <h2>Welcome!</h2>
+        <h4>Enter details to login</h4>
+
+        <form method="post" className="signin">
+            <div >
+                <input type="email"  placeholder='Email' className="inputer" />
+            </div>
+            <div>
+                <input type="password" placeholder='Password' className="inputer" />
+                <a  className="hyperlink">Show</a>
+            </div>
+
+            <h4 >Forgot Password?</h4>
+            <div>
+                <button type='submit' className="inputer">LOG IN</button>
+            </div>
+      </form>
+    </div>
+</div>
+</div>
+    
   );
 }
 
